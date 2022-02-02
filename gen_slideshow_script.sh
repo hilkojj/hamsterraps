375af1535fbd10d17e5065e5acbf5c72332c491e
hilkojj <hilkojj@outlook.com>
2022-02-02 22:06:43 +0100

    slideshow

--- gen_slideshow_script.sh
+++ gen_slideshow_script.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
 
-sudo mount /dev/sda1 /media/photos -o gid=pi,uid=pi
 { echo "const list_str = \`"; find /media/photos/ -type f; echo "\`"; cat slideshow/without_list.js; } > slideshow/with_list.js
 
-python -m src.main_app.boot &
-python -m src.api_server.apiserver
